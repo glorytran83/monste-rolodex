ba022c7028afd9867dea9bfa644c01b8f5035400
Mai Dinh <mandy@Mais-MacBook-Air.local>
2020-03-13 11:44:05 +1100

    Updates

---
+++ precache-manifest.3434857e64d7bd599bb4c3b6a3b31a58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96c83c920e0aa47bc1e9157a6608fa64",
+    "url": "/monste-rolodex/index.html"
+  },
+  {
+    "revision": "a12fd636e6f4d118695a",
+    "url": "/monste-rolodex/static/css/main.3a5dac53.chunk.css"
+  },
+  {
+    "revision": "8a99a4d5f41da4bb1d56",
+    "url": "/monste-rolodex/static/js/2.943f77ce.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monste-rolodex/static/js/2.943f77ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a12fd636e6f4d118695a",
+    "url": "/monste-rolodex/static/js/main.8a70920d.chunk.js"
+  },
+  {
+    "revision": "dba0e202b79061c69abd",
+    "url": "/monste-rolodex/static/js/runtime-main.780dc153.js"
+  }
+]);
